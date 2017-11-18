9196ba36148ec42f7e17127f3e8e6c5b0bb5b571
ccn <c.clausen@imi.de>
2017-11-18 14:44:19 +0100

    add example to MinionProject

--- src/MinionProject.java
+++ src/MinionProject.java
@@ -7,7 +7,17 @@ public class MinionProject {
 
     public static void main(String[] args) {
 
+        int zahl1 = 43;
+        int zahl2 = 5;
+
+        int ergebnis;
+
         System.out.println("Hello Minions!");
+
+        ergebnis = zahl1 + zahl2;
+
+        System.out.println("Das Ergebnis von " + zahl1 + " + " + zahl2 + " ist " + ergebnis + ".");
+
 
     }
 
