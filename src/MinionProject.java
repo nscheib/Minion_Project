547bae5d4fe8aace76c8f60992b1b53a1a6ee57f
Nick <nick.scheib@student.hs-rm.de>
2017-11-19 14:54:56 +0100

    Grober Entwurf für den Anfang. Hatte grad die Muse dazu, aber die Schleife sollten wir am besten zusammen ausarbeiten :)

--- src/MinionProject.java
+++ src/MinionProject.java
@@ -1,22 +1,64 @@
 /*  Import  */
 import java.util.*;
+// Import //
 
-//Test
 
 public class MinionProject {
 
+
     public static void main(String[] args) {
 
-        int zahl1 = 43;
-        int zahl2 = 5;
 
-        int ergebnis;
+        // Variablen
+        int hubert;
+        int auslosungDesBeginners;
+        int auslosungDesSpielers;
+        int spielerMinionWahlAnzahl;
+        char spielerMinionWahlSeite;
+        int auswahlDesComputers;
 
-        System.out.println("Hello Minions!");
+        // Befehl für Scanner 'hans'
+        Scanner hans = new Scanner(System.in);
 
-        ergebnis = zahl1 + zahl2;
 
-        System.out.println("Das Ergebnis von " + zahl1 + " + " + zahl2 + " ist " + ergebnis + ".");
+        // Random Platzierung von Hubert zwischen 2 und 9. Dargestellt als O
+        hubert = (int)(Math.random() * 7) + 2;
+
+
+        // Auslosung, ob Spieler oder Computer startet
+        System.out.println("Um zu ermitteln, ob Sie oder der Computer beginnt, wählen Sie zwischen der Zahl 1 und 2 und geben Sie diese ein:");
+        auslosungDesBeginners = (int)(Math.random() * 2+1);
+        auslosungDesSpielers = hans.nextInt();
+        // Test der Random-Funktion
+        System.out.println("Test: RandomFunktion = " + auslosungDesBeginners);
+
+
+//------??????----------------------------------------------------------------------------------------------------------------------------------------------
+        //
+        if (auslosungDesSpielers == auslosungDesBeginners || auslosungDesSpielers <= auslosungDesBeginners ){
+
+            // Anweisung für Spieler / Eingabe des Spielers
+            System.out.println("Wählen Sie einen, zwei oder drei Minions von der linken oder rechten Seite ausgehend aus.");
+            System.out.println("Geben Sie die Anzahl der Minions an: ");
+            spielerMinionWahlAnzahl = hans.nextInt();
+            System.out.println("Von welcher Seite aus soll gezählt werden? Geben Sie für rechts ein r und für links ein l ein: ");
+            spielerMinionWahlSeite = hans.next().charAt(0);
+
+        }else{
+
+            //Computer beginnt und wählt eine zufällige Anzahl an Minions.
+            System.out.println("Der Computer wählt: ");
+            auswahlDesComputers =(int)(Math.random()*3+1);
+
+        }/*else if(auslosungDesSpielers >= 2) {
+
+            //return und frage erneut nach einer Zahl zwischen 1 und 2
+            return;
+
+        }*/
+//------??????----------------------------------------------------------------------------------------------------------------------------------------------
+
+        System.out.println("Test: hier steht Hubert: " + hubert);
 
 
     }
