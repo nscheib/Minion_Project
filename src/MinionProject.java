b4e8b7bc25d0fcfbba83c3fbbb307dc71b518406
Nick <nick.scheib@student.hs-rm.de>
2017-11-23 18:15:56 +0100

    v.01

--- src/MinionProject.java
+++ src/MinionProject.java
@@ -5,100 +5,113 @@ import java.util.*;
 
 public class MinionProject {
 
-
+    //------MainMethode----------------------------------------------------------------------------------------------------------------------------------------------
     public static void main(String[] args) {
-
 
         // Variablen
         int norbert;
+        int rechteSeite;
+        int linkeSeite;
+        int beginner;
+        boolean playerstart = false;
+        char seitenEingabe;
+        int zahlEingabe;
+        int ausgabeLinks;
+        int ausgabeRechts;
 
-        int MinionPosition;
+        // Position von Norbert
+        norbert = (int) (Math.random() * 11) + 1;
 
-        int auslosungDesBeginners;
-
-        int minionWahlAnzahl;
-        char minionWahlSeite;
-
-        int auswahlDesComputers;
-        int anzahlMinionDesComputers;
-
-        int minionsRechts;
-        int minionsLinks;
-
-        // Befehl für Scanner 'hans'
+        // Scanner
         Scanner hans = new Scanner(System.in);
 
+        //* Test Ausgabe Position Norbert *//
+        System.out.println("Test PositionNorbert: " + norbert);
 
-        // Random Platzierung von Norbert zwischen 1 und 11.
-        norbert = (int)(Math.random() * 10) + 1;
+        // Anzahl der Minions von der rechten Seite
+        rechteSeite = norbert - 1;
 
+        // Anzahl der Minions von der linken Seite
+        linkeSeite = 11 - norbert;
 
-        // Auslosung, ob Spieler oder Computer startet
-        auslosungDesBeginners = (int)(Math.random() * 2+1);
-        if (auslosungDesBeginners == 1){
-            System.out.println("Der Computer fängt an.");
-        }else {
-            System.out.println("Du fängst an.");
+        //* Test Ausgabe links/rechts
+        System.out.println("Test links/rechts: " + linkeSeite + " " + rechteSeite);
+
+        for (int k = 1; k < rechteSeite; k++){
+            System.out.print("X");
+        }
+        System.out.print("O");
+        for (int j = 1; j < linkeSeite; j++){
+            System.out.print("X");
         }
 
+        // Ermittlung des Beginners
+        beginner = (int)(Math.random() * 2) + 1;
 
+        //Begin der Schleife
+        while (norbert != 0) {
 
-//------??????----------------------------------------------------------------------------------------------------------------------------------------------
-
-        // Ausgabe für Position Minion und Norbert
-//        System.out.println("Norbert steht an Stelle: " + norbert);
-//        for (int i=1; i<=11; i++) {
-//                if (i == norbert) {
-//                    System.out.print(" O ");
-//                } else {
-//                    System.out.print(" M ");
-//                }
-//        }
-//        System.out.println("");
-
-        //minionsLinks = norbert <
-        while (norbert != 0);{
-            if(norbert == norbert){
-                System.out.print(" O ");
-            }else if (minionsLinks == minionsLinks && minionsRechts == minionsRechts){
-                System.out.print(" M ");
-            }else if (minionsLinks != minionsLinks && minionsRechts  != minionsRechts){
-                System.out.print(" - ");
+            // Anzeige, ob Pc oder Spieler anfängt
+            if (beginner != 1) {
+                System.out.println("Der Computer beginnt das Match!");
+                playerstart = false;
+            } else {
+                System.out.println("Der Spieler beginnt das Match!");
+                playerstart = true;
             }
 
-            //Abfrage von welcher Seiten begonnen wird
-           if(minionWahlSeite != 'r' || minionWahlSeite != 'l' && minionWahlAnzahl <= 3) {
-                System.out.println("Von welcher Seite möchten Sie wählen? Rechts(r) oder Links (l)");
-                minionWahlSeite = hans.next().charAt(0);
+            // Wenn der Spieler am Zug ist, starte folgenden Ablauf
+            if (playerstart == true) {
 
-                if (minionWahlSeite != 'r' || minionWahlSeite != 'l') {
-                    System.out.println("Geben Sie für Rechts 'r' oder für Links 'l' ein!");
-                    return;
-                } else {
-                    System.out.println("Wie viele Minions möchten Sie wählen?");
-                    minionWahlAnzahl = hans.nextInt();
-                    if (minionWahlAnzahl > 3) {
-                        System.out.println("Bitte geben Sie eine Zahl zwischen 1 und 3 an!");
-                        return;
+                // Spielereingabe mit welcher Seite er beginnen möchte
+                seitenEingabe = hans.next().charAt(0);
+                // Spielereingabe wie viele Minions er wählen möchte
+                zahlEingabe = hans.nextInt();
+
+                // Eingabe des Spielers mit Neueingabe, falls keine richtige Bezeichnung ('r' oder 'l')
+                if (seitenEingabe != 'r' && seitenEingabe != 'l'){
+                    System.out.println("Geben Sie die gewünschte Seite mit 'l' für links und 'r' für rechts: ");
+                    //return seitenEingabe;
+                }
+                // Eingabe des Spielers mit Neueingabe, falls keine richtige Zahl (1 bis 3)
+                if (zahlEingabe <= 3){
+                    System.out.println("Geben Sie die Anzahl an Minions an, mindestens einen und maximal drei: ");
+                    //return zahlEingabe;
+                }
+                // Abziehen der Minions je nach gewählter Seite
+                if (seitenEingabe == 'r'){
+                    ausgabeRechts = zahlEingabe - rechteSeite;
+                }else {
+                    ausgabeLinks = zahlEingabe - linkeSeite;
+                }
+                // Anzeige der Minions
+                for (int i = 1; i < ausgabeLinks; i++) {
+                    if (i == norbert) {
+                        System.out.print("O");
                     } else {
-                        System.out.println("Test");
+                        System.out.print("X");
                     }
+                }
+            // Wenn der Pc am Zug ist, starte folgenden Ablauf
+            }else if (playerstart == false){
+                if (linkeSeite <= 0){
 
                 }
             }
+
+            if (norbert == 0) {
+                if (playerstart == true) {
+                    System.out.println("Sie sind der Gewinner! :)");
+                }else if (playerstart == false) {
+                    System.out.println("Der Computer ist der Gewinner! :(");
+                }
+            }else{
+                    return;
+            }
+
         }
 
 
-        //Abfrage von Minion Anzahl und Seitenbeginn
-        System.out.println("Von welcher Seite möchten Sie wählen? Rechts(r) oder Links (l)");
-        minionWahlSeite = hans.next().charAt(0);
-
-        System.out.println("Wie viele Minions möchten Sie wählen?");
-        minionWahlAnzahl = hans.nextInt();
-
-
-//------??????----------------------------------------------------------------------------------------------------------------------------------------------
     }
-
+//------MainMethode----------------------------------------------------------------------------------------------------------------------------------------------
 }
-
