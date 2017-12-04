6c4b950af7258bd14d318c448ef7fdc909723618
ccn <c.clausen@imi.de>
2017-12-05 00:12:10 +0100

    erste funktionierende Version

--- src/MinionProject.java
+++ src/MinionProject.java
@@ -7,91 +7,104 @@ public class MinionProject {
 
 
         // ------------------------------------------------------------------------------------------
+        //
         // Variablen deklarieren
+        //
         // ------------------------------------------------------------------------------------------
 
-        int anzahlMinions = 11;
-        int positionenVorNorbert;
-        int positionenHinterNorbert;
-        int norbert;
-        int auslosungBeginner;
-        int aktiverSpieler;
+
+        int anzahlMinions = 11;                     // Anzahl der gesamten Minions im Spiel
+
+        int leerePlaetzeLinks = 0;                  // Anzahl leerer Plätze vor Norbert
+        int belegtePlaetzeLinks;                    // Anzahl belegter Plätze vor Norbert
+        int norbert;                                // Norberts Position
+        int belegtePlaetzeRechts;                   // Anzahl belegter Plätze hinter Norbert
+        int leerePlaetzeRechts = 0;                 // Anzahl leerer Plätze hinter Norbert
+
+        int auslosungBeginner;                      // Auslosung des zu startenden Spielers: random 1 oder 2
+        int aktiverSpieler;                         // Zuweisung des aktuellen Spielers
         int computer;
         int spieler;
         boolean gameover = false;
-        char seitenWahl;
+        char seitenWahl;                            // 'r' oder 'l' für die Wahl der Seite
         int computerSeitenWahl;
-        int anzahlGewaehlteMinions;
-        int computerAnzahlGewaehlteMinions;
+        int anzahlGewaehlteMinions;                 // Anzahl der gewählten Minions 1, 2 oder 3
+
+
+
 
 
         // ------------------------------------------------------------------------------------------
-        // Norbert zufällige Position zuweisen
+        //
+        //      Norbert zufällige Position zuweisen
+        //
         // ------------------------------------------------------------------------------------------
 
-        norbert = ((int) (Math.random() * anzahlMinions) + 1);
+
+        norbert = ((int) (Math.random() * anzahlMinions) + 1);                              // weist Norbert einen zufälligen Wert zwischen 1 und 11 zu
         System.out.println("\nNorberts Position: " + norbert);
 
-        positionenVorNorbert = norbert - 1;
-        positionenHinterNorbert = anzahlMinions - norbert;
+        belegtePlaetzeLinks = norbert - 1;                                                  // berechnet die Plätze auf der linken Seite von Norbert
+        belegtePlaetzeRechts = anzahlMinions - norbert;                                     // berechnet die Plätze auf der rechten Seite von Norbert
 
-        System.out.println("Positionen vor Norbert: " + positionenVorNorbert);
-        System.out.println("Positionen hinter Norbert: " + positionenHinterNorbert + "\n");
+        System.out.println("Positionen vor Norbert: " + belegtePlaetzeLinks);
+        System.out.println("Positionen hinter Norbert: " + belegtePlaetzeRechts + "\n");
 
 
-        // Positionen der Minions und von Norbert "graphisch" darstellen
+        // Anfangspositionen der Minions "graphisch" darstellen
 
-        for (int i = 1; i <= positionenVorNorbert; i++) {
+        // Minions links von Norbert
+        for (int i = 1; i <= belegtePlaetzeLinks; i++) {
             System.out.print("M ");
         }
 
+        // Norbert
         System.out.print("O ");
 
-        for (int i = 1; i <= positionenHinterNorbert; i++) {
+        // Minion rechts von Norbert
+        for (int i = 1; i <= belegtePlaetzeRechts; i++) {
             System.out.print("M ");
         }
 
 
+
         // ------------------------------------------------------------------------------------------
-        // Auslosung, wer das Spiel beginnt
+        //
+        //      Auslosung, wer das Spiel beginnt
+        //
         // ------------------------------------------------------------------------------------------
+
 
         auslosungBeginner = ((int) (Math.random() * 2) + 1);
-
         spieler = 1;
         computer = 2;
 
-        if (auslosungBeginner == spieler) {
-
+        if (auslosungBeginner == 1) {
             aktiverSpieler = spieler;
-            System.out.println("\n\n" + "Du fängst an!\n");
-
         } else {
-
             aktiverSpieler = computer;
-            System.out.print("\n\n" + "Der Computer fängt an!\n");
-
         }
 
 
+
         // ------------------------------------------------------------------------------------------
-        // Spiel-Schleife: Spieler wählen abwechselnd 1-3 Minions ins Team, bis Norbert gewählt wird.
+        //
+        //      Spiel-Schleife: Spieler wählen abwechselnd 1-3 Minions ins Team, bis Norbert gewählt wird.
+        //
         // ------------------------------------------------------------------------------------------
+
+
+        Scanner scanner = new Scanner(System.in);
 
 
         while (!gameover) {
 
+            if (aktiverSpieler == spieler) {
 
-            // ------------------------------------------------------------------------------------------
-            // Wenn Spieler = aktiver Spieler:
-            // ------------------------------------------------------------------------------------------
+                System.out.println("\n\nDu bist dran.");
 
-            if (aktiverSpieler == spieler ) {
 
-                // Seite wählen (rechts oder links)
-
-                Scanner scanner = new Scanner(System.in);
-
+                // Seite wählen
                 do {
                     System.out.print("\nVon welcher Seite möchtest du deine Minions wählen? \nGib 'l' für links oder 'r' für rechts ein: ");
                     seitenWahl = scanner.next().charAt(0);
@@ -99,236 +112,115 @@ public class MinionProject {
 
 
                 // Anzahl der Minions wählen
-
                 do {
                     System.out.print("\nWie viele Minions möchtest du wählen? \nGib 1, 2 oder 3 ein: ");
                     anzahlGewaehlteMinions = scanner.nextInt();
                 } while (anzahlGewaehlteMinions > 3 || anzahlGewaehlteMinions < 1);
 
 
-
-                // Positionen der restlichen Minions darstellen
-
-                if (seitenWahl == 'l') {
-
-                    // Gewählte Minions (als Strich (-) dargestellt)
-
-                    for (int i = 1; i <= anzahlGewaehlteMinions; i++) {
-                        System.out.print("- ");
-                    }
-
-                    positionenVorNorbert = positionenVorNorbert - anzahlGewaehlteMinions;   // neuer Wert für positionenVorNorbert setzen
-
-                    // restliche Minions vor Norbert
-
-                    for (int i = 1; i <= positionenVorNorbert; i++) {
-                        System.out.print("M ");
-                    }
-
-                    // Norbert (falls noch da)
-
-                    if (norbert <= anzahlGewaehlteMinions) {
-                        //System.out.println("Du hast verloren, denn du hast Norbert in dein Team gewählt!");
-                        gameover = true;
-                        break;
-                    } else {
-                        System.out.print("O ");
-                    }
-
-                    // Minions hinter Norbert
-
-                    for (int i = 1; i <= positionenHinterNorbert; i++) {
-                        System.out.print("M ");
-                    }
-
-
-                } else {
-
-                    // Minions vor Norbert
-
-                    for (int i = 1; i <= positionenVorNorbert; i++) {
-                        System.out.print("M ");
-                    }
-
-                    // Norbert (falls noch da)
-
-                    if (norbert >= anzahlMinions - anzahlGewaehlteMinions) {
-                        System.out.println("\nDu hast verloren, denn du hast Norbert in dein Team gewählt!");
-                    } else {
-                        System.out.print("O ");
-                    }
-
-                    positionenHinterNorbert = positionenHinterNorbert - anzahlGewaehlteMinions;     // neuer Wert für positionenHinterNorbert setzen
-
-                    // restliche Minions hinter Norbert
-
-                    for (int i = 1; i <= positionenHinterNorbert; i++) {
-                        System.out.print("M ");
-                    }
-
-                    // Gewählte Minions (als Strich (-) dargestellt)
-
-                    for (int i = 1; i <= anzahlGewaehlteMinions; i++) {
-                        System.out.print("- ");
-                    }
-
-                }
-
-
-            // ------------------------------------------------------------------------------------------
-            // Wenn Computer = aktiver Spieler:
-            // ------------------------------------------------------------------------------------------
-
             } else {
 
-                // Seite (rechts oder links) und Anzahl der Minions zufällig wählen
+                System.out.println("\n\nDer Computer ist am Zug.");
 
+
+                // Seite zufällig wählen
                 computerSeitenWahl = ((int) (Math.random() * 2) + 1);
-                computerAnzahlGewaehlteMinions = ((int) (Math.random() * 3) + 1);
-
                 if (computerSeitenWahl == 1) {
                     seitenWahl = 'l';
                 } else {
                     seitenWahl = 'r';
                 }
 
-                if (computerAnzahlGewaehlteMinions == 1) {
-                    anzahlGewaehlteMinions = 1;
-                } else if (computerAnzahlGewaehlteMinions == 2) {
-                    anzahlGewaehlteMinions = 2;
-                } else {
-                    anzahlGewaehlteMinions = 3;
-                }
-
-                System.out.println("\nDer Computer hat " + anzahlGewaehlteMinions + " Minions von " + seitenWahl + " gewählt.");
-
-
-                // Positionen der restlichen Minions darstellen
-
-                if (seitenWahl == 'l') {
-
-                    // Gewählte Minions (als Strich (-) dargestellt)
-
-                    for(int i = 1; i <= anzahlGewaehlteMinions; i++) {
-                        System.out.print("- ");
-                    }
-
-                    positionenVorNorbert = positionenVorNorbert - anzahlGewaehlteMinions;   // neuer Wert für positionenVorNorbert setzen
-
-                    // restliche Minions vor Norbert
-
-                    for(int i = 1; i <= positionenVorNorbert; i++) {
-                        System.out.print("M ");
-                    }
-
-                    // Norbert (falls noch da)
-
-                    if (norbert <= anzahlGewaehlteMinions) {
-                        System.out.println("O ");
-                    } else {
-                        System.out.print("");
-                    }
-
-                    // Minions hinter Norbert
-
-                    for (int i = 1; i <= positionenHinterNorbert; i++) {
-                        System.out.print("M ");
-                    }
-
-
-                } else {
-
-                    // Minions vor Norbert
-
-                    for (int i = 1; i <= positionenVorNorbert; i++) {
-                        System.out.print("M ");
-                    }
-
-                    // Norbert (falls noch da)
-
-                    if (norbert > (anzahlMinions - anzahlGewaehlteMinions)) {
-                        System.out.print("O ");
-                    } else {
-                        System.out.print("");
-                    }
-
-                    positionenHinterNorbert = positionenHinterNorbert - anzahlGewaehlteMinions;     // neuer Wert für positionenHinterNorbert setzen
-
-                    // restliche Minions hinter Norbert
-
-                    for (int i = 1; i <= positionenHinterNorbert; i++) {
-                        System.out.print("M ");
-                    }
-
-                    // Gewählte Minions (als Strich (-) dargestellt)
-
-                    for (int i = 1; i <= anzahlGewaehlteMinions; i++) {
-                        System.out.print("- ");
-                    }
-
-                }
-
+                // Anzahl der Minions zufällig wählen
+                anzahlGewaehlteMinions = ((int) (Math.random() * 3) + 1);
 
             }
 
+            System.out.println("");
+
+
 
             // ------------------------------------------------------------------------------------------
-            // Checken, ob Gameover, ansonsten Spielerwechsel
+            //
+            //      Ausgabe der neuen Positionen der Minions
+            //
             // ------------------------------------------------------------------------------------------
 
-            if (aktiverSpieler == computer) {
 
-                if (seitenWahl == 'l' && norbert <= anzahlGewaehlteMinions) {
-
-                    System.out.println("Der Computer hat verloren, denn er hat Norbert in sein Team gewaehlt.");
-                    gameover = true;
-
-                } else if (seitenWahl == 'r' && norbert >= (anzahlMinions - anzahlGewaehlteMinions)) {
-
-                    System.out.println("Der Computer hat verloren, denn er hat Norbert in sein Team gewaehlt.");
-                    gameover = true;
-
-                }
-
+            if (seitenWahl == 'l') {
+                leerePlaetzeLinks = leerePlaetzeLinks + anzahlGewaehlteMinions;
+                belegtePlaetzeLinks = belegtePlaetzeLinks - anzahlGewaehlteMinions;
             } else {
+                leerePlaetzeRechts = leerePlaetzeRechts + anzahlGewaehlteMinions;
+                belegtePlaetzeRechts = belegtePlaetzeRechts - anzahlGewaehlteMinions;
+            }
 
-                if (seitenWahl == 'l' && norbert <= anzahlGewaehlteMinions) {
+            // leere Plätze links von Norbert
+            for (int i = 1; i <= leerePlaetzeLinks; i++) {
+                System.out.print("- ");
+            }
 
-                    System.out.println("Du hast verloren, denn du hast Norbert in dein Team gewaehlt.");
-                    gameover = true;
+            // belegte Plätze links von Norbert
+            for (int i = 1; i <= belegtePlaetzeLinks; i++) {
+                System.out.print("M ");
+            }
 
-                } else if (seitenWahl == 'r' && norbert >= (anzahlMinions - anzahlGewaehlteMinions)) {
+            // Norbert
+            System.out.print("O ");
 
-                    System.out.println("Du hast verloren, denn du hast Norbert in dein Team gewaehlt.");
-                    gameover = true;
+            // belegte Plätze rechts von Norbert
+            for (int i = 1; i <= belegtePlaetzeRechts; i++) {
+                System.out.print("M ");
+            }
 
+            // leere Plätze rechts von Norbert
+            for (int i = 1; i <= leerePlaetzeRechts; i++) {
+                System.out.print("- ");
+            }
+
+            System.out.print("\n\nSeite: " + seitenWahl + "\nAnzahl der Minions: " + anzahlGewaehlteMinions);
+
+
+
+            // ------------------------------------------------------------------------------------------
+            //
+            // Überprüfen, ob Norbert gewählt wurde und somit das Spiel verloren ist
+            //
+            // ------------------------------------------------------------------------------------------
+
+
+            if ((seitenWahl == 'l' && norbert <= leerePlaetzeLinks) || (seitenWahl == 'r' && anzahlMinions - norbert <= leerePlaetzeRechts)) {
+                if (aktiverSpieler == 2) {
+                    System.out.println("\nDu hast verloren, da du Norbert ins Team gewählt hast.");
+                } else {
+                    System.out.println("\nDu hast gewonnen, da der Computer Norbert ins Team gewählt hat.");
                 }
-
+                gameover = true;
             }
 
 
             // ------------------------------------------------------------------------------------------
-            // Spielerwechsel
+            //
+            //      Spielerwechsel
+            //
             // ------------------------------------------------------------------------------------------
+
 
             if (aktiverSpieler == spieler) {
 
                 aktiverSpieler = computer;
 
-            } else if (aktiverSpieler == computer) {
+            } else {
 
                 aktiverSpieler = spieler;
 
             }
 
-
-        } // while Spiel-Schleife
-
+        } // while
 
 
 
     }  // main method
-
 
 
 
